abe4b7010cb903fe35845e6b4a0a0b4cbf859ab9
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-11-11 07:05:29 +0700

    [http] add webhooks mode with aiohttp

--- app/__main__.py
+++ app/__main__.py
@@ -2,11 +2,28 @@ import asyncio
 import logging
 import sys
 
+import click
+
 from app import bot
 
 
-async def main():
-    await bot.run()
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx: click.Context):
+    if ctx.invoked_subcommand is None:
+        run()
+
+
+@cli.command()
+def run():
+    asyncio.run(bot.run())
+
+
+@cli.command()
+@click.argument("url")
+def set_webhook(url: str):
+    asyncio.run(bot.set_webhook(url))
+    click.echo("Webhook set")
 
 
 if __name__ == "__main__":
@@ -16,4 +33,5 @@ if __name__ == "__main__":
         format="%(asctime)s,%(msecs)03d %(name)-16s %(levelname)-8s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    asyncio.run(main())
+
+    cli()
--- app/config/config.py
+++ app/config/config.py
@@ -1,5 +1,13 @@
-from dataclasses import dataclass
 import os
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Http:
+    host: str
+    port: int
+    webhook_path: Optional[str]
 
 
 @dataclass
@@ -11,20 +19,28 @@ class Redis:
 @dataclass
 class Telegram:
     token: str
+    webhook_url: Optional[str]
 
 
 @dataclass
 class Config:
+    http: Http
     redis: Redis
     telegram: Telegram
 
 
 config = Config(
+    http=Http(
+        host=os.environ.get("HTTP__HOST", "0.0.0.0"),
+        port=int(os.environ.get("HTTP__PORT", "8000")),
+        webhook_path=os.environ.get("HTTP__WEBHOOK_PATH", None),
+    ),
     redis=Redis(
         url=os.environ.get("REDIS__URL", "redis://localhost:6379"),
         prefix=os.environ.get("REDIS__PREFIX", "bot-005"),
     ),
     telegram=Telegram(
         token=os.environ.get("TELEGRAM__TOKEN", ""),
+        webhook_url=os.environ.get("TELEGRAM__WEBHOOK_URL", None),
     ),
 )
